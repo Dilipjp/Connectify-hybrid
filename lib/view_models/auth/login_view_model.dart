6c065bd777c30a335da8447e5963b2a220a3cd33
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-09-22 16:15:29 -0400

    working on login page

---
+++ lib/view_models/auth/login_view_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:connectify/screens/mainscreen.dart';
+import 'package:connectify/services/auth_service.dart';
+import 'package:connectify/utils/validation.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class LoginViewModel extends ChangeNotifier {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
