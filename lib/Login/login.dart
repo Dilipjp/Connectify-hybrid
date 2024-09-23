4a4f4f590a75370778ae59d393a7f8c297006fca
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-09-23 13:14:45 -0400

    working on dependent modules

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:loading_overlay/loading_overlay.dart';
 import 'package:provider/provider.dart';
-import 'package:connectify/register/register.dart';
+import 'package:connectify/Register/register.dart';
 import 'package:connectify/components/password_text_field.dart';
 import 'package:connectify/components/text_form_builder.dart';
 import 'package:connectify/utils/validation.dart';
