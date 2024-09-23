da1b6d5841a347e971037a82539cad4a2be9c210
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-09-23 11:35:49 -0400

    working on Registration

---
+++ lib/view_models/auth/register_view_model.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Registration {
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  bool validate = false;
+  bool loading = false;
+}
