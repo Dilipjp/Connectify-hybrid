356015c199015e45cffe29e2303ee78af92c143f
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-09-23 11:47:12 -0400

    created a model for Registration

--- lib/view_models/auth/register_view_model.dart
+++ lib/view_models/auth/register_view_model.dart
@@ -3,10 +3,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Registration {
+class Registration extends ChangeNotifier {
 
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   bool validate = false;
   bool loading = false;
+  String? Firstname,LastName,email,country,password,cpassword,phnum,gender;
+
+  FocusNode FirstnameFN = FocusNode();
+  FocusNode LastnameFN = FocusNode();
+  FocusNode emailFN = FocusNode();
+  FocusNode countryFN = FocusNode();
+  FocusNode passwordFN = FocusNode();
+  FocusNode cpasswordFN = FocusNode();
+  FocusNode phnumFN = FocusNode();
+  FocusNode genderFN = FocusNode();
+
+
+
 }
