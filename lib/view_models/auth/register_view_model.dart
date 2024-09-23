0e01725614b2f13f4d7743eee3b3597d395ffae1
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-09-23 11:55:16 -0400

    created a model for Registration

--- lib/view_models/auth/register_view_model.dart
+++ lib/view_models/auth/register_view_model.dart
@@ -2,24 +2,64 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth_platform_interface/src/firebase_auth_exception.dart';
+import 'package:flutter/foundation.dart';
+import 'package:connectify/services/auth_service.dart';
 class Registration extends ChangeNotifier {
 
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   bool validate = false;
   bool loading = false;
-  String? Firstname,LastName,email,country,password,cpassword,phnum,gender;
+  String? Firstname,LastName,email,country,password,cPassword,phnum,gender;
 
   FocusNode FirstnameFN = FocusNode();
   FocusNode LastnameFN = FocusNode();
   FocusNode emailFN = FocusNode();
   FocusNode countryFN = FocusNode();
   FocusNode passwordFN = FocusNode();
-  FocusNode cpasswordFN = FocusNode();
+  FocusNode cPasswordFN = FocusNode();
   FocusNode phnumFN = FocusNode();
   FocusNode genderFN = FocusNode();
 
+  register(BuildContext context) async {
+    FormState form = formKey.currentState!;
+    form.save();
 
+    if (!form.validate()) {
+      validate = true;
+      notifyListeners();
+      showInSnackBar(
+          'All feilds are mandatory.', context);
+    }
+    else {
+      if (password == cPassword) {
+        loading = true;
+        notifyListeners();
+        try {
+          bool success = await auth.createUser(
+            firstname: Firstname,
+            lastname: LastName,
+            email: email,
+            password: password,
+            country: country,
+            phnum: phnum,
+            gender: gender,
+          );
+        }
+        catch (e) {
+          loading = false;
+          notifyListeners();
+          print(e);
+          showInSnackBar(
+              '${auth.handleFirebaseAuthError(e.toString() as FirebaseAuthException)}', context);
+        }
+        loading = false;
+        notifyListeners();
+      } else {
+        showInSnackBar('The passwords does not match', context);
+      }
+    }
+  }
 
 }
