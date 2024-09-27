a1df3a0bf6f72c61eb3e38ac30b117c6c1f8914e
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-09-27 14:55:00 -0400

    created file_utils to mmanage sizes of the uploaded image

--- lib/utils/file_utils.dart
+++ lib/utils/file_utils.dart
@@ -12,3 +12,12 @@ class FileUtils {
   static String getFileSize(File file){
     return (file.lengthSync() / (1024 * 1024)).toStringAsFixed(1);
   }
+
+  static String formatBytes(bytes, decimals){
+
+    if (bytes == 0) return "0.0 KB";
+    var k = 1024,
+        dm = decimals <= 0 ? 0 : decimals,
+        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        i = (log(bytes) / log(k)).floor();
+  }}
