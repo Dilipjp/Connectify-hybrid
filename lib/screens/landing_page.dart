f844e651360206b0079a1d1bc789677cc2583c2a
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-09-27 14:34:28 -0400

    working on landing page

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -13,6 +13,28 @@ class Landing extends StatefulWidget {
 class _LandingState extends State<Landing> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+      child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+      Image.asset(
+      'assets/images/new1.png',
+      height: 200.0,
+      width: 200.0,
+      fit: BoxFit.cover,
+    ),
+    SizedBox(height: 20),
+    Text(
+    'Connectify',
+    style: TextStyle(
+    fontSize: 22.0,
+    fontWeight: FontWeight.w900,
+    fontFamily: 'Ubuntu-Regular',
+    ),
+    ),
+    ],
+    ),
+    ),
   }
 }
