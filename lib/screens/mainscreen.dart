ed38dc1160ce146dd57992d424031ca5c1733200
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-09-30 17:49:21 -0400

    working on main screen

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -1,18 +1,10 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:animations/animations.dart';
-import 'package:ionicons/ionicons.dart';
-import 'package:connectify/components/fab_container.dart';
-import 'package:connectify/pages/notification.dart';
-import 'package:connectify/pages/profile.dart';
-import 'package:connectify/pages/search.dart';
-import 'package:connectify/pages/feeds.dart';
-import 'package:connectify/utils/firebase.dart';
+
 
 
 class TabScreen extends StatefulWidget {
-  const TabScreen({super.key});
 
   @override
   State<TabScreen> createState() => _TabScreenState();
@@ -20,57 +12,25 @@ class TabScreen extends StatefulWidget {
 
 class _TabScreenState extends State<TabScreen> {
 
-  int _page = 0;
-
-  List pages = [
-    {
-      'title': 'Home',
-      'icon': Ionicons.home,
-      'page': Feeds(),
-      'index': 0,
-    },
-    {
-      'title': 'Search',
-      'icon': Ionicons.search,
-      'page': Search(),
-      'index': 1,
-    },
-    {
-      'title': 'unsee',
-      'icon': Ionicons.add_circle,
-      'page': Text('nes'),
-      'index': 2,
-    },
-    {
-      'title': 'Notification',
-      'icon': CupertinoIcons.bell_solid,
-      'page': Activities(),
-      'index': 3,
-    },
-    {
-      'title': 'Profile',
-      'icon': CupertinoIcons.person_fill,
-      'page': Profile(profileId: firebaseAuth.currentUser!.uid),
-      'index': 4,
-    },
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageTransitionSwitcher(
-      transitionBuilder: (
-      Widget child,
-      Animation<double> animation,
-      Animation<double> secondaryAnimation,
-    ){
-      return FadeThroughTransition(
-        animation: animation,
-        secondaryAnimation: secondaryAnimation,
-        child: child,
-      );
-    },
-    child: pages[_page]['page'],
-    ),
+      appBar: AppBar(
+        title: Text("Home"), // Optional: You can set the title for the AppBar
+      ),
+      body: Center(
+        child: Text(
+          "This is your home page",
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+
+    );
   }
+
+
 }
