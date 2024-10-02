dd7888f286c7d2599837ebde49c1646bf316bf1c
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-10-02 04:26:16 -0400

    working on main screen view

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -7,6 +7,43 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+
+  int _page = 0;
+
+  List pages = [
+    {
+      'title': 'Home',
+      'icon': Ionicons.home,
+      'page': Feeds(),
+      'index': 0,
+    },
+    {
+      'title': 'Search',
+      'icon': Ionicons.search,
+      'page': Search(),
+      'index': 1,
+    },
+    {
+      'title': 'unsee',
+      'icon': Ionicons.add_circle,
+      'page': Text('nes'),
+      'index': 2,
+    },
+    {
+      'title': 'Notification',
+      'icon': CupertinoIcons.bell_solid,
+      'page': Activities(),
+      'index': 3,
+    },
+    {
+      'title': 'Profile',
+      'icon': CupertinoIcons.person_fill,
+      'page': Profile(profileId: firebaseAuth.currentUser!.uid),
+      'index': 4,
+    },
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
