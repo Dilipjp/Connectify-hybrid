7aae9e532a05fcdbe07c0eb5ffcf9e0db0bd59a0
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-10-04 17:50:21 -0400

    working on posttab

---
+++ lib/screens/followers_tab.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+
+
+class FollowersTab extends StatefulWidget {
+  const FollowersTab({super.key});
+
+  @override
+  State<FollowersTab> createState() => _FollowersTabState();
+}
+
+class _FollowersTabState extends State<FollowersTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/post_tab.dart
+++ lib/screens/post_tab.dart
@@ -1,8 +1,37 @@
-import 'package:flutter/material.dart';
 
-class PostTab extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+class PostTab extends StatefulWidget {
+  const PostTab({super.key});
+
+  @override
+  State<PostTab> createState() => _PostTabState();
+}
+
+class _PostTabState extends State<PostTab> {
+
+  final picker = ImagePicker();
+  File? _image;
+  final TextEditingController captionController = TextEditingController();
+  bool _isLoading = false;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  Future<void> pickImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Post Tab'));
+    return const Placeholder();
   }
 }
+
