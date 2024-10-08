a39337f5a52e061ea46f35c6b04d324e1533db76
Dilip <dilip@Dilips-MacBook-Air.local>
2024-10-08 15:26:29 -0400

    folder stucture changed and screenshot added

---
+++ lib/view_models/auth/register_view_model.dart
@@ -0,0 +1,111 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth_platform_interface/src/firebase_auth_exception.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../services/auth_service.dart';
+
+
+
+class RegisterViewModel extends ChangeNotifier {
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  bool validate = false;
+  bool loading = false;
+  String? Firstname,Lastname,email,country,password,cPassword,phnum,gender;
+
+  FocusNode FirstnameFN = FocusNode();
+  FocusNode LastnameFN = FocusNode();
+  FocusNode emailFN = FocusNode();
+  FocusNode countryFN = FocusNode();
+  FocusNode passwordFN = FocusNode();
+  FocusNode cPasswordFN = FocusNode();
+  FocusNode phnumFN = FocusNode();
+  FocusNode genderFN = FocusNode();
+  AuthService auth = AuthService();
+
+
+  register(BuildContext context) async {
+    FormState form = formKey.currentState!;
+    form.save();
+
+    if (!form.validate()) {
+      validate = true;
+      notifyListeners();
+      showInSnackBar(
+          'All feilds are mandatory.', context);
+    }
+    else {
+      if (password == cPassword) {
+        loading = true;
+        notifyListeners();
+        try {
+          bool success = await auth.createUser(
+            firstname: Firstname,
+            lastname: Lastname,
+            email: email,
+            password: password,
+            country: country,
+            phnum: phnum,
+            gender: gender,
+          );
+        }
+        catch (e) {
+          loading = false;
+          notifyListeners();
+          print(e);
+          showInSnackBar(
+              '${auth.handleFirebaseAuthError(e.toString() as FirebaseAuthException)}', context);
+        }
+        loading = false;
+        notifyListeners();
+      } else {
+        showInSnackBar('The passwords does not match', context);
+      }
+    }
+  }
+
+  setFname(val) {
+    Firstname = val;
+    notifyListeners();
+  }
+
+  setLname(val) {
+    Lastname = val;
+    notifyListeners();
+  }
+  setEmail(val) {
+    email = val;
+    notifyListeners();
+  }
+
+  setPassword(val) {
+    password = val;
+    notifyListeners();
+  }
+  setConfirmPass(val) {
+    cPassword = val;
+    notifyListeners();
+  }
+
+  setCountry(val) {
+    country = val;
+    notifyListeners();
+  }
+  setGender(val) {
+    gender = val;
+    notifyListeners();
+  }
+  setPhoneNum(val) {
+    phnum = val;
+    notifyListeners();
+  }
+
+  void showInSnackBar(String value, context) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(value)));
+  }
+
+}
